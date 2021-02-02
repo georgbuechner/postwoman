a1750900c22efb127abcf1c36ac7702483d60868
fux <jan.dick@bdp.org>
2021-02-02 20:00:24 +0100

    Fixes postwoman

--- postwoman.sh
+++ postwoman.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-path_to_scheduler=$(/home/fux/Documents/programming/postwoman/src/scheduler.py)
-path_to_configs=$(/home/fux/Documents/programming/postwoman/configs)
 
 file=$(find $path_to_scheduler -type f -name "*.yaml" | dmenu -i -l 35)
 outtput=$(python3 $path_to_scheduler $file)
