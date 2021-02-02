8d63338673b16e08c7576ec7843d60e515f9cdcd
fux <jan.dick@bdp.org>
2021-02-02 20:00:00 +0100

    Fixes init

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sed -i '2ipath_to_configs=$('$PWD'/configs)' postwoman.sh
+sed -i '2ipath_to_scheduler=$('$PWD'/src/scheduler.py)' postwoman.sh
 cp postwoman.sh /usr/bin/postwoman.sh
 mkdir results
--- postwoman.sh
+++ postwoman.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+path_to_scheduler=$(/home/fux/Documents/programming/postwoman/src/scheduler.py)
 path_to_configs=$(/home/fux/Documents/programming/postwoman/configs)
-path_to_configs=$(/home/fwork/Documents/programming/postwoman/configs)
-path_to_configs=$(/home/fwork/Documents/programming/postwoman/configs)
 
-file=$(find $path_to_configs -type f -name "*.yaml" | dmenu -i -l 35)
-outtput=$(python3 src/schedular.py $file)
+file=$(find $path_to_scheduler -type f -name "*.yaml" | dmenu -i -l 35)
+outtput=$(python3 $path_to_scheduler $file)
 notify-send "$outtput"
 nvim "${file/configs/results}"    
